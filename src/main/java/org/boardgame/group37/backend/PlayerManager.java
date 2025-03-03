4b982a7ce72db738d42fb489623bf14fe42777f7
Ole-Kristian <olemofo@gmail.com>
2025-03-03 15:26:10 +0100

    player

--- src/main/java/org/boardgame/group37/backend/PlayerManager.java
+++ src/main/java/org/boardgame/group37/backend/PlayerManager.java
@@ -1,5 +1,26 @@
 package org.boardgame.group37.backend;
+import java.util.ArrayList;
 
 public class PlayerManager {
 
+    // Properties
+    private ArrayList<Player> players = new ArrayList<>();
+
+    // Constructor
+    public PlayerManager() {
+
+    }
+
+    // Methods
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void playerAdd() {
+        players.add(new Player());
+    }
+
+    public void playerRemove() {
+        players.remove(players.size() - 1);
+    }
 }
