43828b0c38e42125ecb30a694b1d7ce0701651ef
Ole-Kristian <olemofo@gmail.com>
2025-03-03 14:40:01 +0100

    die and dieManager

--- src/main/java/org/boardgame/group37/backend/Die.java
+++ src/main/java/org/boardgame/group37/backend/Die.java
@@ -1,6 +1,22 @@
 package org.boardgame.group37.backend;
 
 public class Die {
+
+    // Properties
     private int value = 1;
     
+    // Contstructors
+    public Die() {
+    }
+    public Die(int value){
+        this.value = value;
+    }
+
+    // Methods
+    public int getValue() {
+        return value;
+    }
+    public void executeThrow() {
+        value = (int) (Math.random() * 6) + 1;
+    }
 }
--- src/main/java/org/boardgame/group37/backend/DieManager.java
+++ src/main/java/org/boardgame/group37/backend/DieManager.java
@@ -1,5 +1,32 @@
 package org.boardgame.group37.backend;
+import java.util.ArrayList;
 
 public class DieManager {
 
+    // Properties
+    private ArrayList<Die> dice = new ArrayList<>();
+
+    // Constructor
+    public DieManager() {
+        
+    }
+
+    // Methods
+    public void dieAdd() {
+        dice.add(new Die());
+    }
+
+    public void dieRemove() {
+        dice.remove(dice.size()-1);
+    }
+
+    public void diceThrow() {
+        for(Die d : dice) {
+            d.executeThrow();
+        }
+    }
+
+    public int diceValue() {
+        return dice.stream().mapToInt((x) -> x.getValue()).sum();
+    }
 }
