a8e015b48e217ea5e24791f3fbf53cffc5a36265
Ole-Kristian <olemofo@gmail.com>
2025-03-03 15:27:35 +0100

    init for the day

--- src/main/java/org/boardgame/group37/backend/Tile.java
+++ src/main/java/org/boardgame/group37/backend/Tile.java
@@ -1,11 +1,17 @@
 package org.boardgame.group37.backend;
 
 public class Tile {
+
+    // Properties
     private Action action;
+
+    // Constructor
     public Tile() {
         this.action = new ActionDefault();
     }
     public Tile(Action action) {
         this.action = action;
     }
+
+    // Methods
 }
