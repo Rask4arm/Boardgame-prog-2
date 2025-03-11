30189086ef9196f350fd50aba502b82494f02b4f
Ole-Kristian <olemofo@gmail.com>
2025-03-11 14:20:54 +0100

    working on file handling

--- src/main/java/org/boardgame/group37/backend/PlayerDataManager.java
+++ src/main/java/org/boardgame/group37/backend/PlayerDataManager.java
@@ -8,10 +8,33 @@ import com.opencsv.CSVWriter;
 public class PlayerDataManager {
 
     public PlayerDataPackage dataLoad(String playerName) {
+        try {
+            CSVReader reader = new CSVReader(Files.newBufferedReader(Paths.get("data/players.csv")));
+            String[] data;
+            PlayerDataPackage dataReturn = null;
+            while ((data = reader.readNext()) != null) {
+                if (data[0].equals(playerName)) {
+                    dataReturn = new PlayerDataPackage(data[0], Color.decode(data[1]), Integer.parseInt(data[2]));
+                }
+            }
+            reader.close();
+            return dataReturn;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     public void dataSave(PlayerDataPackage playerData) {
+        try {
+            CSVWriter writer = new CSVWriter(Files.newBufferedWriter(Paths.get("data/players.csv"), StandardOpenOption.APPEND));
+            String[] data = {playerData.getName(), Integer.toString(playerData.getColor().hashCode()), Integer.toString(playerData.getWins())};
+            writer.writeNext(data);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return;
     }
 
